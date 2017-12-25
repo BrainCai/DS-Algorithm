9d20bb13bf7a5cbe5b74f259dc04fd16f25385d9
caiqiang <caiqiang@bonc.com.cn>
2017-12-25 16:36:08 +0800

    最大子数组和

---
+++ src/main/java/array/FindGreatestSumOfSubArray.java
@@ -0,0 +1,48 @@
+package array;
+
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+/**
+ * HZ偶尔会拿些专业问题来忽悠那些非计算机专业的同学。今天测试组开完会后, 他又发话了:在古老的一维模式识别中, 常常需要计算连续子向量的最大和,当向量全为正数的时候,问题很好解决。
+ * 但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。你会不会被他忽悠住？(子向量的长度至少是1)
+ *
+ * @author caiqiang
+ * @version 2017年12月25日
+ * @see FindGreatestSumOfSubArray
+ * @since
+ */
+public class FindGreatestSumOfSubArray {
+    public int findGreatestSumOfSubArray(int[] array) {
+        if (array.length <= 0) {
+            return 0;
+        }
+        int sum = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++ ) {
+            sum = 0;
+            for (int j = i; j < array.length; j++ ) {
+                sum = sum + array[j];
+                list.add(sum);
+            }
+
+        }
+        int minSum = Integer.MIN_VALUE;
+        for (Integer integer : list) {
+            if (minSum < integer) {
+                minSum = integer;
+            }
+        }
+
+        return minSum;
+    }
+
+    @Test
+    public void test() {
+        int[] array = {1, -2, 3, 10, -4, 7, 2, -5};
+        System.out.println(findGreatestSumOfSubArray(array));
+    }
+}
