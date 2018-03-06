16cb52eebb93c811ab0c1f7e0e7c389929f656ab
“1974310692@qq.com <1974310692@qq.com>
2018-03-06 12:03:26 +0800

    调整代码的风格

--- src/main/java/dp/base/recurion/ArrayLoop.java
+++ src/main/java/dp/base/recurion/ArrayLoop.java
@@ -12,48 +12,49 @@ import org.junit.Test;
  */
 public class ArrayLoop {
 
-    public void loop(int[] array) {
-	if (array.length == 0) {
-	    return;
-	}
-	_loop(0, array);
-    }
-
-    /**
-     * 递归实现
-     * 
-     * @param i
-     * @param array
-     */
-    private void _loop(int i, int[] array) {
-	if (i >= array.length) {
-	    return;
-	}
-	System.out.println(array[i]);
-	_loop(i + 1, array);
-    }
-
-    private int sum(int[] array) {
-	if (array.length == 0) {
-	    return 0;
-	}
-	return _sum(0, array);
-    }
-
-    private int _sum(int i, int[] array) {
-
-	if (i >= array.length) {
-	    return 0;
+	public void loop(int[] array) {
+		if (array.length == 0) {
+			return;
+		}
+		_loop(0, array);
 	}
 
-	return _sum(i + 1, array) + array[i];
-    }
+	/**
+	 * 递归实现
+	 * 
+	 * @param i
+	 * @param array
+	 */
+	private void _loop(int i, int[] array) {
 
-    @Test
-    public void test() {
-	int[] array = new int[] { 2, 3, 1, 5, 9 };
-	// loop(array);
-	System.out.println(sum(array));
-    }
+		if (i >= array.length) {
+			return;
+		}
+		System.out.println(array[i]);
+		_loop(i + 1, array);
+	}
+
+	private int sum(int[] array) {
+		if (array.length == 0) {
+			return 0;
+		}
+		return _sum(0, array);
+	}
+
+	private int _sum(int i, int[] array) {
+
+		if (i >= array.length) {
+			return 0;
+		}
+
+		return _sum(i + 1, array) + array[i];
+	}
+
+	@Test
+	public void test() {
+		int[] array = new int[] { 2, 3, 1, 5, 9 };
+		// loop(array);
+		System.out.println(sum(array));
+	}
 
 }
