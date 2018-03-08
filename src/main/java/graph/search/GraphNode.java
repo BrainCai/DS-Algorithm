0b4368a48f18705aa9ce2d1130fa12181adf1f34
“1974310692@qq.com <1974310692@qq.com>
2018-03-08 20:28:20 +0800

    图顶点结构定义

---
+++ src/main/java/graph/search/GraphNode.java
@@ -0,0 +1,37 @@
+package graph.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 图顶点的定义
+ * 
+ * @author BrianCai
+ *
+ */
+public class GraphNode {
+	public List<GraphEdge> edgeList = null;
+
+	private String label = "";
+
+	public GraphNode(String label) {
+		this.label = label;
+		if (edgeList == null) {
+			edgeList = new ArrayList<GraphEdge>();
+		}
+	}
+
+	/**
+	 * 给当前节点添加一条边 GraphNode
+	 * 
+	 * @param edge
+	 *            添加的边
+	 */
+	public void addEdgeList(GraphEdge edge) {
+		edgeList.add(edge);
+	}
+
+	public String getLabel() {
+		return label;
+	}
+}
