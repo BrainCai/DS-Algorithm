d2cda6672b75b9a29c6f6b92faca0da7bf5577fb
“1974310692@qq.com <1974310692@qq.com>
2018-03-09 09:33:08 +0800

    添加信息打印工具类

---
+++ src/main/java/utile/PrintMessageUtil.java
@@ -0,0 +1,13 @@
+package utile;
+
+public class PrintMessageUtil {
+
+	public static void printMesssageWithoutNewLines(String message) {
+		System.out.print(message);
+	}
+
+	public static void printMesssageWithNewLines(String message) {
+		System.out.println(message);
+	}
+
+}
