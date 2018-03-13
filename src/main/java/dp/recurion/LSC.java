bf7dbcb331456844dcbb7711d0b691f46261b92d
“1974310692@qq.com <1974310692@qq.com>
2018-03-13 17:38:51 +0800

    规范代码style

--- src/main/java/dp/recurion/LSC.java
+++ src/main/java/dp/recurion/LSC.java
@@ -1,4 +1,4 @@
-package dp;
+package dp.recurion;
 
 import org.junit.Test;
 
@@ -23,34 +23,35 @@ public class LSC {
 		char[] charArray1 = str1.toCharArray();
 		char[] charArray2 = str2.toCharArray();
 
-		return LSC(charArray1, charArray2, 0, 0);
+		return lsc(charArray1, charArray2, 0, 0);
 
 	}
 
-	private int LSC(char[] charArray1, char[] charArray2, int idx1, int idx2) {
+	private int lsc(char[] charArray1, char[] charArray2, int idx1, int idx2) {
 
 		if (idx1 >= charArray1.length || idx2 >= charArray2.length) {
 			return 0;
 		}
 
 		if (charArray1[idx1] == charArray2[idx2]) {
-			return 1 + LSC(charArray1, charArray2, idx1 + 1, idx2 + 1);
+			return 1 + lsc(charArray1, charArray2, idx1 + 1, idx2 + 1);
 		}
 
-		return Math.max(LSC(charArray1, charArray2, idx1 + 1, idx2), LSC(charArray1, charArray2, idx1, idx2 + 1));
+		return Math.max(lsc(charArray1, charArray2, idx1 + 1, idx2), lsc(charArray1, charArray2, idx1, idx2 + 1));
 	}
 
 	@Test
 	public void test() {
-		// System.out.println(longestSquence("ABCBDAB", "BDCABA"));
-		char[] charArray1 = "ABCBDAB".toCharArray();
-		char[] charArray2 = "BDCABA".toCharArray();
-		System.out.println(LSC_1(charArray1, charArray2, 0, 0));
+		System.out.println(longestSquence("ABCBDAB", "BDCABA"));
+		// char[] charArray1 = "ABCBDAB".toCharArray();
+		// char[] charArray2 = "BDCABA".toCharArray();
+		// System.out.println(LSC_1(charArray1, charArray2, 0, 0));
 	}
 
 	int max = 0;
 
 	/**
+	 * ´í½â
 	 * 
 	 * @param charArray1
 	 * @param charArray2
